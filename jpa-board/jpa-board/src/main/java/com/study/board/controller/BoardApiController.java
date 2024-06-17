0b579936eac2593075e3c071a3970d9e410ee25f
Sang <rsw7876@naver.com>
2024-06-17 22:15:50 +0900

    exception

--- jpa-board/jpa-board/src/main/java/com/study/board/controller/BoardApiController.java
+++ jpa-board/jpa-board/src/main/java/com/study/board/controller/BoardApiController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.study.exception.CustomException;
+import com.study.exception.ErrorCode;
+
 // API Ã³¸®¿ë BoardApiController
 @RestController
 @RequestMapping("/api")
@@ -11,7 +14,8 @@ public class BoardApiController {
 	
 	@GetMapping("/test")
 	public String test() {
-		throw new RuntimeException("Exception...");
+//		throw new RuntimeException("Exception...");
+		throw new CustomException(ErrorCode.POST_NOT_FOUND);
 	}
 
 }
