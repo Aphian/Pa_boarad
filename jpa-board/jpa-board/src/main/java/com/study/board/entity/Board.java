ade3815052a5036fda005efb9e08b299c5fc4999
Sang <rsw7876@naver.com>
2024-06-18 22:04:28 +0900

    board_entity

--- jpa-board/jpa-board/src/main/java/com/study/board/entity/Board.java
+++ jpa-board/jpa-board/src/main/java/com/study/board/entity/Board.java
@@ -1,5 +1,49 @@
 package com.study.board.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+	private String content;
+	private String writer;
+	private int hits;
+	private char deleteYn;
+	
+	private LocalDateTime createdDate = LocalDateTime.now();
+	private LocalDateTime modifiedDate;
+	
+	@Builder
+	public Board(String title, String content, String writer, int hits, char deleteYn) {
+		this.title = title;
+		this.content = content;
+		this.writer = writer;
+		this.hits = hits;
+		this.deleteYn = deleteYn;
+	}
+	
+	public void update(String title, String content, String writer) {
+		this.title = title;
+		this.content = content;
+		this.writer = writer;
+		this.modifiedDate = LocalDateTime.now();
+	}
 
 }
