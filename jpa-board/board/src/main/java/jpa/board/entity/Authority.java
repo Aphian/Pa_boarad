71ff1b72649e2bd54ad5bee4c47948bf3f5f9ac2
Sang <rsw7876@naver.com>
2024-07-03 22:16:23 +0900

    JPA_Entity

---
+++ jpa-board/board/src/main/java/jpa/board/entity/Authority.java
@@ -0,0 +1,7 @@
+package jpa.board.entity;
+
+public enum Authority {
+	
+	ROLE_USER, ROLE_ADMIN;
+	
+}
---
+++ jpa-board/board/src/main/java/jpa/board/entity/Board.java
@@ -0,0 +1,63 @@
+package jpa.board.entity;
+
+//import jpa.board.dto.BoardDto;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "board_id")
+	private Long id;
+	
+	private String title;
+	private String content;
+	
+	@CreatedDate
+	private LocalDateTime regDate;
+	
+	@LastModifiedDate
+	private LocalDateTime uptDate;
+	
+	private Long viewCount;
+	private String delYn;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+	
+	public Board update(String title, String content) {
+		this.title = title;
+		this.content = content;
+		return this;
+	}
+	
+	public Board delete(String delYn) {
+		this.delYn = delYn;
+		return this;
+	}
+	
+	@Builder
+	public Board(String title, String content, Member member) {
+		this.title = title;
+		this.content = content;
+		this.viewCount = 0L;
+		this.delYn = "N";
+		this.member = member;
+	}
+
+}
