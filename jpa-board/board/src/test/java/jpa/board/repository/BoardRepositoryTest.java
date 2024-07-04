da1875c41c6ee4383ef62bb8b545f1667efa3d8a
Sang <rsw7876@naver.com>
2024-07-04 22:45:31 +0900

    board_repository

---
+++ jpa-board/board/src/test/java/jpa/board/repository/BoardRepositoryTest.java
@@ -0,0 +1,16 @@
+package jpa.board.repository;
+
+//import jpa.board.dto.BoardDto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoardRepositoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
