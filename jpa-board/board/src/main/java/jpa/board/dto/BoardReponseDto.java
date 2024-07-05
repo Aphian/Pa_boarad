f9b2dc598b2a95b729a7eb14920e0fde048e6ba0
Sang <rsw7876@naver.com>
2024-07-05 22:06:19 +0900

    jpa_board_dto

--- jpa-board/board/src/main/java/jpa/board/dto/BoardReponseDto.java
+++ jpa-board/board/src/main/java/jpa/board/dto/BoardReponseDto.java
@@ -1,5 +1,34 @@
 package jpa.board.dto;
 
+import java.time.LocalDateTime;
+
+import jpa.board.entity.Board;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
 public class BoardReponseDto {
+	
+	private Long id;
+	private String title;
+	private String content;
+	private LocalDateTime regDate;
+	private LocalDateTime uptDate;
+	private Long viewCount;
+	private String delYn;
+	private String username;
+	
+	public BoardReponseDto(Board board) {
+		this.id = board.getId();
+		this.title = board.getTitle();
+		this.content = board.getContent();
+		this.regDate = board.getRegDate();
+		this.uptDate = board.getUptDate();
+		this.viewCount = board.getViewCount();
+		this.delYn = board.getDelYn();
+		this.username = board.getMember().getUsername();
+	}
 
 }
--- jpa-board/board/src/main/java/jpa/board/dto/BoardRequestDto.java
+++ jpa-board/board/src/main/java/jpa/board/dto/BoardRequestDto.java
@@ -1,5 +1,16 @@
 package jpa.board.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class BoardRequestDto {
+	
+	private String title;
+	private String content;
+	private Long memberId;
 
 }
