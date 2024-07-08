edda492114ab72f45c7c850cc0a6370b146c3fa2
Sang <rsw7876@naver.com>
2024-07-08 12:48:06 +0900

    board_dto_modify

--- jpa-board/board/src/main/java/jpa/board/dto/BoardRequestDto.java
+++ jpa-board/board/src/main/java/jpa/board/dto/BoardRequestDto.java
@@ -6,11 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import jakarta.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class BoardRequestDto {
 	
+	@NotEmpty(message = "제목은 필수입니다.")
 	private String title;
 	private String content;
 	private Long memberId;
