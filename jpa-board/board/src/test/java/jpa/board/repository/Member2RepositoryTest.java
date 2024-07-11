0b9961d66d4f2ba6fcef235b2946b8513536c944
Sang <rsw7876@naver.com>
2024-07-11 14:11:07 +0900

    member_test

---
+++ jpa-board/board/src/test/java/jpa/board/repository/Member2RepositoryTest.java
@@ -0,0 +1,14 @@
+package jpa.board.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Member2RepositoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
