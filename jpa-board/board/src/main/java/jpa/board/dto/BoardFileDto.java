097c6a105b4f64322377a168abf22139aef24ff3
Sang <rsw7876@naver.com>
2024-07-19 13:19:47 +0900

    jpa_boardFileDto

--- jpa-board/board/src/main/java/jpa/board/dto/BoardFileDto.java
+++ jpa-board/board/src/main/java/jpa/board/dto/BoardFileDto.java
@@ -12,10 +12,20 @@ import java.time.LocalDateTime;
 
 @Data
 public class BoardFileDto {
+	
+	private Long boardFileId;
 
     private Long id;
+    
+    private Long fileId;
 
     private Long boardId;
+    
+    private String originFileName;
+    
+    private Long size;
+    
+    private String extension;
 
     public BoardFileDto(){
 
@@ -25,7 +35,17 @@ public class BoardFileDto {
     public BoardFileDto(Long boardId){
         this.boardId = boardId;
     }
-
+    
+    @QueryProjection
+    public BoardFileDto(Long boardFileId, Long fileId, String originFileName, Long size, String extension) {
+    	this.boardFileId = boardFileId;
+    	this.fileId = fileId;
+    	this.originFileName = originFileName;
+    	this.size = size;
+    	this.extension = extension;
+    }
+    
+    
     public BoardFile toEntity(File file){
         return BoardFile.builder()
                 .boardId(boardId)
