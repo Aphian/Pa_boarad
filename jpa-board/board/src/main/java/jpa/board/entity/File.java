4e65f8ba48b6dc554652353f3268cd9cda7fbdd6
Sang <rsw7876@naver.com>
2024-07-22 16:55:49 +0900

    jpa_persistence_modify

--- jpa-board/board/src/main/java/jpa/board/entity/File.java
+++ jpa-board/board/src/main/java/jpa/board/entity/File.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 public class File {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "file_id")
 	private Long id;
 	
