7b313b619b24638051b674741d33edacfb39e13c
Sang <rsw7876@naver.com>
2024-08-19 14:46:57 +0900

    memberDto

--- jpa-board/board/src/main/java/jpa/board/dto/Member2Dto.java
+++ jpa-board/board/src/main/java/jpa/board/dto/Member2Dto.java
@@ -1,5 +1,17 @@
 package jpa.board.dto;
 
+import java.time.LocalDateTime;
+import com.querydsl.core.annotations.QueryProjection;
+
+import jpa.board.entity.Board;
+import lombok.Builder;
+import lombok.Data;
+
+import jakarta.validation.constraints.NotEmpty;
+
+@Data
 public class Member2Dto {
+	
+	private Long id;
 
 }
