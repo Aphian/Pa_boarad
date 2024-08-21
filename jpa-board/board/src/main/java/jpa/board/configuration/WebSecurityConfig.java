81d8bb4efa1eff1edcadf28e2b602e8c1011d2d0
Sang <rsw7876@naver.com>
2024-08-21 21:52:07 +0900

    jpa_board_java_security

--- jpa-board/board/src/main/java/jpa/board/configuration/WebSecurityConfig.java
+++ jpa-board/board/src/main/java/jpa/board/configuration/WebSecurityConfig.java
@@ -1,51 +1,51 @@
-//package jpa.board.configuration;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-//
-//import lombok.RequiredArgsConstructor;
-//
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//public class WebSecurityConfig {
-//	
-//	private final AuthenticationFailureHandler customFailureHandler;
-//	
-//	@Bean
-//	public BCryptPasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();		
-//	}
-//	
-//	@Bean
-//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//		http.authorizeHttpRequests((authorize) -> authorize
-//				.requestMatchers("/post").hasRole("USER")
-//				.requestMatchers("/admin/**").hasRole("ADMIN")
-//				.anyRequest().permitAll()
-//			)
-//			.formLogin((formLogin) -> formLogin
-//					.loginPage("/members/login")
-//					.loginProcessingUrl("/loginProc")
-//					.usernameParameter("username")
-//					.passwordParameter("password")
-//					.defaultSuccessUrl("/")
-//					.failureHandler(customFailureHandler)
-//					.permitAll()
-//			)
-//			.logout((logout) -> logout
-//					.logoutUrl("/logoutProc")
-//					.logoutSuccessUrl("/")
-//					.permitAll()
-//			)
-//			.csrf((csrf) -> csrf.disable());
-//		
-//		return http.build();
-//	}
-//
-//}
+package jpa.board.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+	
+	private final AuthenticationFailureHandler customFailureHandler;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();		
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authorize) -> authorize
+				.requestMatchers("/post").hasRole("USER")
+				.requestMatchers("/admin/**").hasRole("ADMIN")
+				.anyRequest().permitAll()
+			)
+			.formLogin((formLogin) -> formLogin
+					.loginPage("/members/login")
+					.loginProcessingUrl("/loginProc")
+					.usernameParameter("username")
+					.passwordParameter("password")
+					.defaultSuccessUrl("/")
+					.failureHandler(customFailureHandler)
+					.permitAll()
+			)
+			.logout((logout) -> logout
+					.logoutUrl("/logoutProc")
+					.logoutSuccessUrl("/")
+					.permitAll()
+			)
+			.csrf((csrf) -> csrf.disable());
+		
+		return http.build();
+	}
+
+}
