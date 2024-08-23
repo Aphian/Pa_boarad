9d5524cd04fefc9bbf392b233adb7235bce0dd14
Sang <rsw7876@naver.com>
2024-08-23 15:41:32 +0900

    jpa_board_controller

--- jpa-board/board/src/main/java/jpa/board/controller/Member2Controller.java
+++ jpa-board/board/src/main/java/jpa/board/controller/Member2Controller.java
@@ -1,6 +1,7 @@
 package jpa.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import jpa.board.dto.Member2Dto;
 import jpa.board.entity.Member2;
@@ -12,5 +13,15 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 public class Member2Controller {
+	
+	private final Member2Service member2Service;
+	
+	@GetMapping("/")
+	public String login() {
+		
+		
+		return "members/login";
+		
+	}
 
 }
