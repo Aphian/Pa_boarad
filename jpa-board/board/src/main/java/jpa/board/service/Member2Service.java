83939b137c0113f892ad3e641c9f814970e7a097
Sang <rsw7876@naver.com>
2024-08-26 21:51:23 +0900

    jpa_board_service

--- jpa-board/board/src/main/java/jpa/board/service/Member2Service.java
+++ jpa-board/board/src/main/java/jpa/board/service/Member2Service.java
@@ -7,6 +7,13 @@ import jpa.board.repository.Member2Repository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class Member2Service {
 
 }
