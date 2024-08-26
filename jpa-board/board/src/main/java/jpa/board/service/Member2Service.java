db64fa744464b6908a5d5a90409ccfa9d950801e
Sang <rsw7876@naver.com>
2024-08-26 21:39:14 +0900

    jap_board

--- jpa-board/board/src/main/java/jpa/board/service/Member2Service.java
+++ jpa-board/board/src/main/java/jpa/board/service/Member2Service.java
@@ -1,5 +1,12 @@
 package jpa.board.service;
 
+import jpa.board.dto.Member2Dto;
+import jpa.board.entity.Member2;
+import jpa.board.repository.Member2Repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 public class Member2Service {
 
 }
