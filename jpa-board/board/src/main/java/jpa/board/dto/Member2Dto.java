72a16517d22c2fc9d21b47b1f06c381de472d755
Sang <rsw7876@naver.com>
2024-08-28 22:06:19 +0900

    jpa_board_memberDto

--- jpa-board/board/src/main/java/jpa/board/dto/Member2Dto.java
+++ jpa-board/board/src/main/java/jpa/board/dto/Member2Dto.java
@@ -17,7 +17,10 @@ public class Member2Dto {
 	
 	@NotEmpty(message = "아이디를 입력해주세요.")
 	private String username;
+	
+	@NotEmpty(message = "비밀번호를 입력해주세요.")
 	private String password;
+	
 	private String phoneNo;
 	private int age;
 	private Authority authority;
--- jpa-board/board/src/main/java/jpa/board/entity/Member2.java
+++ jpa-board/board/src/main/java/jpa/board/entity/Member2.java
@@ -17,7 +17,7 @@ public class Member2 {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;            
-    private String username;    
+    private String username;
     private String password;    
     private String phoneNo;     
     private int age;         
